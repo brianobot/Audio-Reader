97d24ddaaa76271a46fa44cafdcf9fc2852d49b5
Brian Obot <brianobot9@gmail.com>
2022-09-07 15:59:51 +0100

    Bug Fixes

--- reader.py
+++ reader.py
@@ -1,12 +1,18 @@
 import pyttsx3
 import PyPDF2
 import sys
+import yaml
 
 speaker = pyttsx3.init()
 rate = speaker.setProperty('rate', 165)
 
+#to create a sensation of relation, the user can change the reader's name 
+reader_name = "David"
 
-introduction = 'Hello there! I am David. A Software Reader to enable you to listen to your favourite books, Written and owned by Brian'
+introduction = f"""Hello there! I am {reader_name}.
+    A Software Reader to enable you to listen to your favourite books,
+    Written and Maintained by Brian Obot <brianobot9@gmail.com>"""
+
 speaker.say(introduction)
 speaker.runAndWait()
 
@@ -18,7 +24,7 @@ def main(file_name):
             speaker.say(data)
             speaker.runAndWait()
     else:
-        with open(file_name) as file:
+        with open(file_name, encoding="utf-8") as file:
             data = file.readlines()
             for line in data:
                 speaker.say(line)
@@ -26,5 +32,12 @@ def main(file_name):
     speaker.stop()
 
 if __name__ == "__main__":
-    file_name = sys.argv[1]
+    try:
+        file_name = sys.argv[1]
+    except:
+        file_name = input("Enter Complete path to File to be read: ")
+        
+    print("_____________________________")
+    print(f"starting {file_name}")
+    print("_____________________________")
     main(file_name)
